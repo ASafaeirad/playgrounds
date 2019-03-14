91815350f6ac627435e7024bb1ad83fcd6c92103
frontendmonster <frontendmonster@gmail.com>
2019-03-14 21:54:56 +0330

    selector: make code more flexible

--- selector.js
+++ selector.js
@@ -1,41 +1,85 @@
-var btnSelect = document.getElementById('btnSelect');
-var txtSelector = document.getElementById('txtSelector');
-var parents = document.getElementById('parents');
+const childs = document.querySelectorAll('.child');
+const parents = document.querySelectorAll('.parent')
+const txtSelector = document.getElementById('selector');
+var timeout;
+
+const createClassSpan = (child, filter = '', className) => {
+  const classSpan = document.createElement('span');
+  const classNames = child
+    .classList
+    .toString()
+    .split(' ')
+    .filter(c => c !== filter)
+    .join(' ');
+
+  classSpan.textContent = `class="${classNames}"`;
+  classSpan.classList.add(className);
+  return classSpan;
+}
+
+const createIdSpan = (child, className) => {
+  const idSpan = document.createElement('span');
+  const id = child.getAttribute('id');
+  console.log(id)
+  if(!id) {
+    return idSpan;
+  }
+
+  idSpan.textContent = `id="${id}"`;
+  idSpan.classList.add(className);
+  return idSpan;
+}
+
+const createTypeSpan = (child) => {
+  const typeSpan = document.createElement('span')
+  typeSpan.textContent = child.tagName.toLowerCase();
+  typeSpan.classList.add('type');
+  return typeSpan;
+}
+
+childs.forEach(child => {
+  const classSpan = createClassSpan(child, 'child');
+  const typeSpan = createTypeSpan(child)
+
+  child.appendChild(typeSpan);
+  child.appendChild(classSpan);
+})
+
+parents.forEach(parent => {
+  const classSpan = createClassSpan(parent, '', 'parent__class');
+  const idSpan = createIdSpan(parent, 'parent__id');
+  const hr = document.createElement('hr');
+  hr.classList.add('parent__hr')
+  parent.prepend(hr);
+  parent.prepend(classSpan);
+  parent.prepend(idSpan);
+});
+
 txtSelector.addEventListener('keyup', delayOnSelect);
-var delay;
+[...childs, ...parents].forEach(_ => _.addEventListener('mouseenter', onSelect));
+[...childs, ...parents].forEach(_ => _.addEventListener('mouseleave', onSelect));
 
-var childs = document.querySelectorAll('.child');
-childs.forEach(_ => _.addEventListener('mouseenter', onSelect))
-childs.forEach(_ => _.addEventListener('mouseleave', onSelect))
+const unSelectAll = () => [...childs, ...parents].forEach(el => el.classList.remove('selected'));
+const selectElements = (els) => els.forEach(el => el.classList.add('selected'));
 
-function delayOnSelect(e) {
-  if(delay) clearTimeout(delay);
-  delay = setTimeout(function() {
-    onSelect(e);
-  }, 500);
+function delayOnSelect() {
+  if (timeout) {
+    clearTimeout(timeout);
+    timeout = null;
+  }
+
+  timeout = setTimeout(onSelect, 500);
 }
 
-function onSelect(e) {
+function onSelect() {
   unSelectAll();
-  if(!txtSelector.value) return;
-  if(txtSelector.value.includes(":hover")) console.log('hover');
+  if (txtSelector.value === '') {
+    return;
+  }
   try {
-    var selectedElements = document.querySelectorAll(txtSelector.value);
-    select(selectedElements);
-  } catch (e) {
-    console.warn('Not Valid');
-  }
-}
-
-function unSelectAll() {
-  var allElemenets = document.querySelectorAll('*')
-  for (el of allElemenets) {
-    el.classList.remove('selected');
-  }
-}
-
-function select(els) {
-  for (el of els) {
-    el.classList.add('selected');
+    const selectedElements = document.querySelectorAll(txtSelector.value);
+    selectElements(selectedElements);
+  } catch(e) {
+    console.warn('Invalid Selector', txtSelector.value)
   }
 }
